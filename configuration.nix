0c7398bb979a2de0694cac784e8d13f549601d27
hegde-atri <iamatrihegde@outlook.com>
2022-12-14 23:04:43 +0000

    added alacritty and badges to readme

--- configuration.nix
+++ configuration.nix
@@ -56,8 +56,8 @@
   services.xserver.enable = true;
 
   # Enable the Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.xfce.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
@@ -97,7 +97,14 @@
     description = "Atri Hegde";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-      firefox neovim logseq pfetch neofetch btop discord spotify git gcc _7zz
+      firefox 
+      neovim 
+      logseq 
+      pfetch neofetch
+      alacritty
+      btop 
+      discord spotify 
+      git gcc _7zz emacs vscode
     ];
   };
 
@@ -110,9 +117,10 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
+  services.auto-cpufreq.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
