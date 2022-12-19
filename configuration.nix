92a1a5d594d1c5deaf6cb75e7e6dbaac93b30bfe
hegde-atri <iamatrihegde@outlook.com>
2022-12-19 11:41:16 +0000

    added hyprland cachix

--- configuration.nix
+++ configuration.nix
@@ -187,6 +187,10 @@
     options = "--delete-older-than 10d";
   };
 
+  nix.settings.substituters = ["https://hyprland.cachix.org"];
+  nix.settings.trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+
+
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
